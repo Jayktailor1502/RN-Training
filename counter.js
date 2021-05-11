158e4d8d50824f1eb5d8d566767774889bc57392
Jayktailor1502 <jay.kumar@unthinkable.co>
2021-05-11 09:23:16 +0530

    Counter variable JS

---
+++ counter.js
@@ -0,0 +1,15 @@
+
+var count = 0;
+
+function myInc(){
+    count++;
+    document.getElementById("place").innerHTML = count;
+}
+
+function myDec(){
+    count--;
+    document.getElementById("place").innerHTML = count;
+}
+
+// -----------------------------------------------------
+
